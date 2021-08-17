a7d9d0d75a3f530c577560feca19d99b2d75c2f1
jinfu <2445349521@qq.com>
2021-08-17 16:54:26 +0800

    第二次

--- src/main/java/org/example/test01.java
+++ src/main/java/org/example/test01.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class test01 {
+    public static void main(String[] args) {
+        System.out.println("test01.....");
+        System.out.println("test01.....");
+        System.out.println("test01.....");
+        System.out.println("test01.....");
+        System.out.println("test01.....");
+
+    }
 }
